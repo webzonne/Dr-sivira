915bd6c3898e53cbce5576e7151f58ff62a15085
hector silva <hectorsf21@gmail.com>
2022-12-13 18:29:48 +0100

    production10

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -13,8 +13,8 @@ export default async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD
+      user: `${process.env.SMTP_USER}`,
+      pass: `${process.env.SMTP_PASSWORD}` 
     }
   });
 
